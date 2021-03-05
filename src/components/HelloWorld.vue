dc452b5d479c7a373defa5ba5cddf7010cc9b590
Gracejean <jennilyn.mendoza@student.passerellesnumeriques.org>
2021-03-04 18:56:31 -0800

    trial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="hello" >
+    <h1>{{ msg }}</h1>
+    <br>
+    <Sayings :color="color"/>
+    <div id="greet">
+    <user-name name="Jean"></user-name>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Sayings from './Sayings'
+//  import Vue from 'vue'
+import Vue from 'vue/dist/vue.js';
+
+    Vue.component('user-name',{
+        props:['name'],
+        template:
+            `<p>Good morning {{name}}</p>`
+    })
+
+    window.onload = function(){
+       new Vue({
+      el:'#greet'
+    })
+    }
+    
+
+
+export default {
+  
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data (){
+    return{
+      color: 'red'
+    }
+  },
+  components: {
+    Sayings,
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Sayings.vue
@@ -0,0 +1,72 @@
+<template>
+  <div >
+    <p>{{ sayings }}</p>
+    <button 
+    v-on:click="reverseMessage">Reversed</button>
+    <br>
+    <input v-model="type" type="text" />
+    <br>
+    <p>{{type}}</p>
+    <span v-bind:title="title"> The visiting hours is over</span>
+    <br>
+    <br>
+    <span v-if="seen">Now you see me</span>
+    <ol>
+        <li v-for="todo in todos" :key="todo.id">
+            {{todo.text}}
+        </li>
+    </ol>
+    <br>
+
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+    var data = {a:2}
+    var obj = {
+        foo:'bar'
+    }
+    Object.freeze(obj)
+   var vm = new Vue({
+       data: {
+           obj: '',
+           b: 8
+       },
+       created (){
+           console.log('b is ' + this.b);
+       }
+       
+       
+   })
+   console.log(vm.a == data.a);
+   console.log(vm.a = 3);
+   console.log(data.a);
+   console.log(obj.foo)
+
+export default {
+  name: "Sayings",
+  data() {
+    return {
+      sayings: "Never give up",
+      type: null,
+      title: 'The date today is ' + new Date().toLocaleString(),
+      seen:false,
+      todos:[
+          {text: 'Study first'},
+          {text: 'Practice more'},
+          {text: 'Persevere'}
+      ]
+    };
+  },
+  methods: {
+    reverseMessage() {
+      this.sayings = this.sayings.split("").reverse("").join("");
+
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
